468521eb65510b9d9aba4add4b3f312a2d074c9f
Dave Wright <dave.wright@gmail.com>
2025-10-17 09:29:58 -0700

    minikube

--- mk_guide.sh
+++ mk_guide.sh
@@ -1,4 +1,9 @@
+# Start
 minikube start
 helm install docker-kube-demo ./helm/docker-kube-demo
 kubectl get pods -l app.kubernetes.io/instance=docker-kube-demo
 minikube service docker-kube-demo-frontend
+
+# Stop
+helm uninstall docker-kube-demo
+minikube stop
